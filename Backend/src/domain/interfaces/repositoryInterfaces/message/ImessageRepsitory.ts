c6f2cd9661cbc92e9c26d4c6f9ef51fa07641bd5
Aparna Shaju <aparnakanatil2002@gmail.com>
2025-09-15 17:41:21 +0530

    create message added

---
+++ Backend/src/domain/interfaces/repositoryInterfaces/message/ImessageRepsitory.ts
@@ -0,0 +1,7 @@
+import { MessageEntity } from "../../../entities/chat/messageEntity"
+
+export interface ImessageRepostiory {
+    createMessage(message: MessageEntity): Promise<MessageEntity>
+    getMessages(senderId: string): Promise<MessageEntity[] | []>
+    getMessagesOfAChat(chatId: string,pageNo:number): Promise<{ messages: MessageEntity[], hasMore: boolean }>
+}
